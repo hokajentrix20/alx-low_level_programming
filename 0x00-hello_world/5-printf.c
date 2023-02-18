9a811163a8daac6bb7971ddb3641bd1cb5a200c7
Hoka <hokajentrix@gmail.com>
2023-02-18 11:22:45 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - using puts
- * 
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	printf("with proper gramas but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
