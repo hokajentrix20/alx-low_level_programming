124bff9c1d28c3d1babfc40ee4e1933c2d91601f
Hoka <hokajentrix@gmail.com>
2023-02-22 12:51:46 -0800

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *main - checks lowercase letters
+ *Return 1 if c is lowercase
+ *Return o if otherwise
+ */
+
+int _is lower(int c)
+
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return(1);
+	}
+		return(0);
+}
