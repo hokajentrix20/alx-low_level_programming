f8e3124b9d6f1dcf1d4cce4d3420909b75c1f5b5
Hoka <hokajentrix@gmail.com>
2023-02-27 10:16:53 -0800

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
+
 /**
- * Print_alphabet - Make alphabets
- * Return: void
+ * print_alphabet - print all alphabet in lowercase
  */
+
 void print_alphabet(void)
-
 {
-	char c;
+	char letter;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
 	_putchar('\n');
 }
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,15 @@
 #include "main.h"
+
 /**
- *main - checks lowercase letters
- *Return 1 if c is lowercase
- *Return o if otherwise
+ * _islower - check if char is lowercase
+ * @c: is the char to be checked
+ * Return: 1 if char is lowercase, otherwise 0.
  */
 
-int _is lower(int c)
-
+int _islower(int c)
 {
-
-	if (c >= 97 && c <= 122)
-	{
-		return(1);
-	}
-		return(0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
